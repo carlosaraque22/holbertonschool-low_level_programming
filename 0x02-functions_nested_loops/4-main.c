310f2cdb10df457ea89818b757e6b13b24af3f13
Carlos Araque <cararqueu@gmail.com>
2020-09-22 15:37:06 +0000

    third project in C

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,4 +1,4 @@
-
+#include "holberton.h"
 
 /**
  * main - check the code for Holberton School students.
@@ -7,13 +7,15 @@
  */
 int main(void)
 {
-int r;
+  int r;
 
-r = _islower('H');
+r = _isalpha('H');
 _putchar(r + '0');
-r = _islower('o');
+r = _isalpha('o');
 _putchar(r + '0');
-r = _islower(108);
+r = _isalpha(108);
+_putchar(r + '0');
+r = _isalpha(';');
 _putchar(r + '0');
 _putchar('\n');
 return (0);
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,8 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-int _putchar(char c);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+int _isalpha(int c);
 #endif
 
