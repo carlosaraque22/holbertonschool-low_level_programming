52e06860b2c300e2bb01fd66b6a1ced0f5853a47
Carlos Araque <cararqueu@gmail.com>
2020-09-22 23:43:20 +0000

    third project in C

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Write a program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded).
- *
- * @i: the name of the variable.
- *
- * @r: the name of the variable.
+ * main - Write a program that computes and prints
  *
  * Return: On success 1.
  */
