fe84e3ee940a42804cbe186962ada5921b41693a
Carlos Araque <cararqueu@gmail.com>
2020-09-22 19:21:01 +0000

    third project in C

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers together.
+ *
+ * @d: the name of the variable.
+ *
+ * @w: the name of the variable.
+ *
+ * Return: the result of the sum of both variables.
+ */
+int add(int d, int w)
+{
+return (d + w);
+}
