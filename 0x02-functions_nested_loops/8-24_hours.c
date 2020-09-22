c25a0dc0bfb4f19aea09584f322c4556043d9932
Carlos Araque <cararqueu@gmail.com>
2020-09-22 18:03:19 +0000

    third project in C

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - Write a function that prints every minute of the day.
+ *
+ * Return: print every minute from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+int d,ca,b,va,apple;
+apple = 9;
+for (d = 0; d <= 2; d++)
+{
+for (ca = 0; ca <= 3; ca++)
+{
+for (b = 0; b <= 5; b++)
+{
+for (va = 0; va <= 9; va++)
+_putchar (d + '0')
+_putchar (ca + '0')
+_putchar (':')
+_putchar (b + '0')
+_putchar (va + '0')
+_putchar ('\n')
+}
+}
+}
+}
+
