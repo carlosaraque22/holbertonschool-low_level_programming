a5d681e5a3aa0b52895f7ae840f9acfa11a5ebda
Carlos Araque <cararqueu@gmail.com>
2020-09-22 16:16:28 +0000

    third project in C

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_sign - function used.
+ *
+ * @n: the name of the variable.
+ *
+ * Return: sometimes 0.
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,9 +1,8 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-#endif
+int print_sign(int n);
+
 
