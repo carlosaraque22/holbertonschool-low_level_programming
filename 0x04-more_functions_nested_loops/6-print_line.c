0fd6158bca4852797a14dc1b59a383bb5e4ca396
Carlos Araque <cararqueu@gmail.com>
2020-09-24 18:04:36 +0000

    more loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_line - print n amount of underscores.
+ *
+ * @n: the amount of underscors.
+ *
+ * Return: is not neccessary.
+ */
+void print_line(int n)
+{
+if (n > 0)
+{
+for (n = n; n > 0; n--)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
