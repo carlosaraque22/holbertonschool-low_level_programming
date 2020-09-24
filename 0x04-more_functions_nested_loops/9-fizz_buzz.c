821aeeeb23daa6f64a1648092be9c0fcde41c740
Carlos Araque <cararqueu@gmail.com>
2020-09-24 20:13:50 +0000

    third project in C

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: void (Success)
+ */
+int main(void)
+{
+int g;
+
+for (g = 1; g <= 100; g++)
+{
+if ((g % 3 == 0) && (g % 5 == 0))
+{
+printf("FizzBuzz");
+}
+else if (g % 3 == 0)
+{
+printf("Fizz");
+}
+else if (g % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%i", g);
+}
+if (g != 100)
+{
+printf(" ");
+}
+}
+printf("\n");
+return (0);
+}
