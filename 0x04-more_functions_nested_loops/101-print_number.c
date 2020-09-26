c3dafc154ea431b6fdb395c8bf58ead473a9711b
Carlos Araque <cararqueu@gmail.com>
2020-09-26 00:25:56 +0000

    third project in C

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_number - Prints an integer.
+ *
+ * @number: The name of the variale.
+ *
+ * Return: is not neccessary.
+ */
+void print_number(int n)
+{
+unsigned int number = n;
+
+if (n < 0)
+{
+_putchar('-');
+number = -number;
+}
+
+if ((number / 10) > 0)
+print_number(number / 10);
+_putchar((number % 10) + '0');
+}
