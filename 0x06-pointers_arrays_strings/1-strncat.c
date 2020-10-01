22980739b71e84a350a5cfa6860b3b7d397677ee
Carlos Araque <cararqueu@gmail.com>
2020-10-01 14:17:43 +0000

    more pointers, arrays and strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,14 @@
 #include "holberton.h"
 
 char *_strncat(char *dest, char *src, int n)
-	
+{
+	int h, i;
+	for (h = 0; dest[h] != '\0'; h++)
+	{
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[h + i] = src[i];
+	}
+	return (dest);
+}
