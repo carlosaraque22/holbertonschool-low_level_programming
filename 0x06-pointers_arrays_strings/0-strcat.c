41ff303eb7e5aa0da375c7c00d23cacd50b54fb0
Carlos Araque <cararqueu@gmail.com>
2020-10-01 15:46:35 +0000

    more pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,15 +1,24 @@
 #include "holberton.h"
 
+/**
+ * _strcat - Appends the first num characters of source to destination.
+ *
+ * @dest: destination string.
+ * @src: source string.
+ *
+ * Return: dest
+ */
+
 char *_strcat(char *dest, char *src)
 {
 	int h, i, a;
-	for (h = 0 ; dest[h] != '\0'; ++h)
+for (h = 0 ; dest[h] != '\0'; ++h)
 		{
 		}
 	for (i = 0 ; src[i] != '\0' ; ++i)
 	{
 	}
-	for ( a = 0; a != i; ++a)
+for (a = 0; a != i ; ++a)
 		{
 			dest[h] = src[a];
 			++h;
