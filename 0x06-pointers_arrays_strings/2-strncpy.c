c22ba1da39d296dd11d0c284331a57c0961586d2
Carlos Araque <cararqueu@gmail.com>
2020-10-01 16:46:46 +0000

    more pointers, arrays and strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,28 +10,16 @@
  * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
-{
-	int h, i, g;
-for (h = 0; src[h] != '\0'; h++)
-{
-}
-if (h < n)
-{
-	for (i = 0; i < h; i++)
-	{
-		dest[i] = src[i];
-	}
-	for (; i < n; i++)
-	{
-		dest[i] = '\0';
-	}
-	}
-else
-{
-	for (g = 0; g < n; g++)
-	{
-		dest[g] = src[g];
-	}
-}
-	return (dest);
-}
+{	
+		 int i;
+
+		 for (i = 0; i < n && src[i] != '\0'; i++)
+			 dest[i] = src[i];
+		 {
+		 }
+		 for ( ; i < n; i++)
+		 {
+			 dest[i] = '\0';
+		 }
+		 return dest;
+	 }
