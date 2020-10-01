44214bb5dc8b697aae5ce698e5e47e67ce7de2cd
Carlos Araque <cararqueu@gmail.com>
2020-10-01 16:04:40 +0000

    more pointers, arrays and strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,27 @@
 #include "holberton.h"
 
+/**
+ * _strncpy - function that copies a string
+ *
+ * @dest: destination string
+ * @src: source string
+ * @n: max byte of src from dest
+ *
+ * Return: dest
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int h, i;
-	for (h = 0; src[i] != '\0'; h++)
-	{
-	}
+int h, i;
+for (h = 0; src[h] != '\0'; h++)
+{
+}
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[] = src[];
+		dest[i] = src[i];
+	}
+	for (; n > h; h++)
+	{
+		dest[i] = '\0';
 	}
 	return (dest);
 }
