20dbc7e51d4deaec27d4d25146fd69a72f00e3b9
Carlos Araque <cararqueu@gmail.com>
2020-10-01 14:28:31 +0000

    more pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int h, i;
+	for (h = 0; src[i] != '\0'; h++)
+	{
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[] = src[];
+	}
+	return (dest);
+}
