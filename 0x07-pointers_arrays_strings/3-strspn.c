e7a770a1871ca1b087db2a16847c7c625e774be3
Carlos Araque <cararqueu@gmail.com>
2020-10-05 15:33:47 +0000

    More pointers

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,22 +10,21 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int j, i, len;
-	len = 0;
-	for(i = 0; s[i] != '\0'; i++)
+	unsigned int j, i, len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-	for(j = 0; accept[j] != '\0'; j++)
+	for (j = 0; accept[j] != '\0'; j++)
 	{
-		if(s[i] == accept[j])
+		if (s[i] == accept[j])
 		{
 			len++;
 		break;
 
 		}
 	}
-		if(s[i] != accept[j])
+		if (s[i] != accept[j])
 		break;
 	}
-	return(len);
+	return (len);
 }
-	    
