ac219e730491fc61978b0c971c8eea47e5b245dd
Carlos Araque <cararqueu@gmail.com>
2020-10-05 15:08:03 +0000

    More pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,21 @@
 #include "holberton.h"
 
+/**
+ * _memset - prints buffer in hexa
+ * @n: the address of memory to print
+ * @s: s
+ * @b: b
+ * Return: s
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 
 {
 	unsigned int i;
-	for(i = 0; i < n; i++, *s++)
+	for(i = 0; i < n; i++, s++)
 	{
 		*s = b;
-		{
-			return (s);
-		}
 	}
+			return (s);
 }
+
