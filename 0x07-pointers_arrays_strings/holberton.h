be506b54d0c0f697753e4cc2307bd09d8732f772
Carlos Araque <cararqueu@gmail.com>
2020-10-05 17:49:46 +0000

    More pointers

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -4,3 +4,4 @@ char *_memset(char *s, char b, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
