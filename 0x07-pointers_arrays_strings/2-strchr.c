f14ee3b21f3279540571cb86cbe1200042a22b88
Carlos Araque <cararqueu@gmail.com>
2020-10-05 15:17:35 +0000

    More pointers

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,17 @@
 #include "holberton.h"
 
+/**
+ * _strchr - check the code for Holberton School students.
+ * @s: a
+ * @c: b
+ * Return: s
+ */
+
 char *_strchr(char *s, char c)
 {
-		while(c != *s && *s != '\0')
-		{
-			s++;
-		}
-	return(s);
+while(c != *s && *s != '\0')
+{
+s++;
+}
+return(s);
 }
