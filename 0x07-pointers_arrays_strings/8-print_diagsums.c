6e3e1163c14154268b00f251b229f9c04ffbbff2
Carlos Araque <cararqueu@gmail.com>
2020-10-06 02:04:20 +0000

    More pointers

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - print the sum of the  diagonals of a matrix
+ * @a: the matrix
+ * @size: the size of matrix
+ *
+ */
+
+void print_diagsums(int *a, int size)
+{
+
+int i = 0, j = 0, diag_one = 0, diag_two = 0;
+
+for (i = 0; i < size * size; i++)
+{
+diag_one += *(a + i);
+i += size;
+}
+for (j = 0; j < size; j++)
+{
+diag_two += *(a + ((size - 1) - j));
+a += size;
+}
+printf("%i, %i\n", diag_one, diag_two);
+}
