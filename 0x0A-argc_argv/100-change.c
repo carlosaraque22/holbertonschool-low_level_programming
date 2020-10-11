e3d0cc994748d1c56881624d8ec9c65ffc813008
Carlos Araque <cararqueu@gmail.com>
2020-10-11 22:51:03 +0000

    argc and argv

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function to print out the change that needs to given.
+ *
+ * @argc: number of arguments.
+ *
+ * @argv: numbero of coins.
+ *
+ * Return: The return is 0 success.
+ */
+
+int main(int argc, char **argv)
+{
+	int total, change;
+
+	if (argc != 2)
+	{
+		return (printf("Error\n"), 1);
+	}
+
+	change = atoi(argv[1]);
+
+	for (total = 0; change > 0; total++)
+	{
+		if (change - 25 >= 0)
+			change = change - 25;
+		else if (change - 10 >= 0)
+			change = change - 10;
+		else if (change - 5 >= 0)
+			change = change - 5;
+		else if (change - 2 >= 0)
+			change = change - 2;
+		else if (change - 1 >= 0)
+			change = change - 1;
+	}
+	printf("%d\n", total);
+	return (0);
+}
