ba3c2cf53c4d8b46345c152f589047020a121e63
Carlos Araque <cararqueu@gmail.com>
2020-10-15 22:37:29 +0000

    more malloc and free

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,67 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * string_nconcat - concatenated 2 string but n bytes of s2.
+ *
+ * @s1: string 1
+ * @s2: string 2
+ * @n: unsigned int
+ *
+ * Return: two strings.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *c;
+	unsigned int sizei, sizeii, i, ii;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	sizei = _strlen(s1);
+	sizeii = _strlen(s2);
+	if (n > sizeii)
+	{
+		n = sizeii;
+	}
+	c = malloc(sizeof(char) * (sizei + n) + 1);
+	if (c == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < sizei; i++)
+	{
+		c[i] = s1[i];
+	}
+	for (ii = 0; ii < n; ii++)
+	{
+		c[i + ii] = s2[ii];
+	}
+	{
+		c[i + ii] = '\0';
+		return (c);
+	}
+}
+
+/**
+ * _strlen - a counter for the char s.
+ *
+ * @s: char.
+ *
+ * Return: the success.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for(i = 0; s[i] != '\0'; i++)
+		return (i);
+}
