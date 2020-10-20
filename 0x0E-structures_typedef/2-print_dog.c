57d73bad4b0d488713cd48054c06cb4f8803c18a
Carlos Araque <cararqueu@gmail.com>
2020-10-20 02:23:53 +0000

    Structures, typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,14 +17,19 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: (nil)\n");
 	}
-	if (d->owner == NULL)
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	{
+		printf("Age: %f\n", d->age);
+	}
+		if (d->owner == NULL)
 	{
 		printf("Owner: (nil)\n");
 	}
 	else
 	{
-		printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner);
 	}
 }
