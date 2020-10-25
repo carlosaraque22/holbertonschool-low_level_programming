6a86e4e8643121b5c79833ffee77ee7f0971211b
Carlos Araque <cararqueu@gmail.com>
2020-10-25 19:38:52 +0000

    variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		separator = "";
 	}
 	va_start(valist, n);
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
 		str = va_arg(valist, char *);
 		if (str != '\0')
