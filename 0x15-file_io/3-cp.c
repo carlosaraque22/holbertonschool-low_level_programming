57e96fe8b243ae0de21dd69b6ad9697694ec1e66
Carlos Araque <cararqueu@gmail.com>
2020-11-09 23:57:12 +0000

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,3 +16,15 @@ void r_error(char *file)
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
 	exit(98);
 }
+
+/**
+ * w_error - Prints the write error and exit.
+ * @file: pointer to a file name.
+ *
+ * Return: Nothing.
+ */
+void w_error(char *file)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+	exit(99);
+}
