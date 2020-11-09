0cd5b36b84ca3282c37ac719a867fe0cbec4ef12
Carlos Araque <cararqueu@gmail.com>
2020-11-09 23:51:03 +0000

    fix header

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,10 +7,11 @@
 
 /**
  * read_textfile - eads a text file and prints it to the POSIX standard output.
- * @filename: name of the file.
+ *
+ * @filename: this is the name of the file.
  * @letters: number of letters it should read and print
  *
- * Return:  number of letters it could read and print or 0 in case of fail.
+ * Return: return number of letters it could read and print, or 0 if fails.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
