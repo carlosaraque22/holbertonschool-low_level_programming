378c40199d190423c21e5b879440fdf978522ef0
Carlos Araque <cararqueu@gmail.com>
2020-11-10 00:01:54 +0000

    fix3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -40,3 +40,54 @@ void c_error(int b)
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", b);
 	exit(100);
 }
+
+/**
+ * main - Entry point.
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int fd1, fd2, w, r = 1, close1;
+	char *buf;
+
+	buf = malloc(1024);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+		r_error(argv[1]);
+	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 00664);
+	if (fd2 == -1)
+		w_error(argv[2]);
+	while (r > 0)
+	{
+		r = read(fd1, buf, 1024);
+		if (r == -1)
+		{
+			free(buf);
+			r_error(argv[1]);
+		}
+		if (r > 0)
+		{
+			w = write(fd2, buf, r);
+			if (w == -1)
+			{
+				free(buf);
+				w_error(argv[2]);
+			}
+		}
+	}
+	close1 = close(fd1);
+	if (close1 == -1)
+		c_error(fd1);
+	close1 = close(fd2);
+	if (close1 == -1)
+		c_error(fd2);
+	return (0);
+}
